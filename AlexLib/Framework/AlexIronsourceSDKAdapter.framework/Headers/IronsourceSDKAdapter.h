173a2cd1a11e8bf24ca8a605fda02380a81386f3
GUO PENG <guopeng@topon.com>
2023-05-25 15:24:13 +0800

    init

---
+++ AlexLib/Framework/AlexIronsourceSDKAdapter.framework/Headers/IronsourceSDKAdapter.h
@@ -0,0 +1,19 @@
+//
+//  IronsourceSDKAdapter.h
+//  IronsourceSDKAdapter
+//
+//  Created by GUO PENG on 2023/5/22.
+//
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IronsourceSDKAdapter.
+FOUNDATION_EXPORT double IronsourceSDKAdapterVersionNumber;
+
+//! Project version string for IronsourceSDKAdapter.
+FOUNDATION_EXPORT const unsigned char IronsourceSDKAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IronsourceSDKAdapter/PublicHeader.h>
+
+
